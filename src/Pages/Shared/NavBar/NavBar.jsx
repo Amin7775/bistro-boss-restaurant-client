92cfba8515489197401da11e4f04b71b70f960a3
Amin7775 <amin.dpi24@gmail.com>
2023-11-15 20:54:41 +0600

    Fixed Nav on top of banner

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-black bg-opacity-25 text-white fixed max-w-screen-xl mx-auto z-10">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
