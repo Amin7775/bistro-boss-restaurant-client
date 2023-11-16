a3ecfd90dd3d9592b83548ff8d0ba8919786f2b8
Amin7775 <amin.dpi24@gmail.com>
2023-11-17 02:18:03 +0600

    Create Custom Hook Usemenu To Load Menu Data

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -2,26 +2,29 @@ import { useEffect, useState } from "react";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import axios from "axios";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 
 const PopularMenu = () => {
-    const [menu,setMenu] = useState();
-    useEffect(()=>{
-        axios.get('./menu.json')
-        .then(data => {
-            const loadedData = data.data
-            const popularItems = loadedData.filter(item => item.category === 'popular')
-            console.log(popularItems)
-            setMenu(popularItems)
-        })
-    },[])
-    console.log('menu :>> ', menu);
+    // const [menu,setMenu] = useState();
+    // useEffect(()=>{
+    //     axios.get('./menu.json')
+    //     .then(data => {
+    //         const loadedData = data.data
+    //         const popularItems = loadedData.filter(item => item.category === 'popular')
+    //         console.log(popularItems)
+    //         setMenu(popularItems)
+    //     })
+    // },[])
+    const [menu]= useMenu();
+    const popular = menu?.filter(item => (item.category) === 'popular')
+    console.log('menu :>> ', popular);
     return (
         <section>
             <SectionTitle Heading={"FROM OUR MENU"} SubHeading={"Check it out"}></SectionTitle>
             <div className="my-5 grid grid-cols-2 gap-4">
                 {
-                    menu?.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                    popular?.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
         </section>
---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useMenu = () => {
+    const [menu,setMenu] = useState();
+    const [loading,setLoading] = useState(true)
+    useEffect(()=>{
+        axios.get('./menu.json')
+        .then(data => {
+            const loadedData = data.data
+            setMenu(loadedData)
+            setLoading(false)
+        })
+    },[])
+    return [menu,loading]
+    
+};
+
+export default useMenu;
