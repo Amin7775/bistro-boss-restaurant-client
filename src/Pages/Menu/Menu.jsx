39d18f3fc3a1a09a50bb46f8e6e56b4f8507a2df
Amin7775 <amin.dpi24@gmail.com>
2023-11-17 17:06:02 +0600

    Display different menu category items with cover

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,18 +1,49 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover/Cover";
-import cover1 from '../../assets/menu/banner3.jpg'
-import PopularMenu from "../Home/PopularMenu/PopularMenu";
+import cover1 from "../../assets/menu/banner3.jpg";
+import DessertImg from "../../assets/menu/dessert-bg.jpeg";
+import PizzaImg from "../../assets/menu/pizza-bg.jpg";
+import SaladImg from "../../assets/menu/salad-bg.jpg";
+import SoupImg from "../../assets/menu/soup-bg.jpg";
+// import PopularMenu from "../Home/PopularMenu/PopularMenu";
+import useMenu from "../../hooks/useMenu";
+import SectionTitle from "../Shared/SectionTitle/SectionTitle";
+import MenuCategory from "./MenuCategory/MenuCategory";
 const Menu = () => {
-    return (
-        <div>
-            <Helmet>
-            <title>Bistro Boss | Menu</title>
-            </Helmet>
-            menu
-            <Cover img={cover1} heading={'Our menu'} subHeading={'Would you like to try a dish?'}></Cover>
-            <PopularMenu></PopularMenu>
-        </div>
-    );
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      {/* Main Cover */}
+      <Cover
+        img={cover1}
+        heading={"Our menu"}
+        subHeading={"Would you like to try a dish?"}
+      ></Cover>
+      {/* Offered Menu */}
+      <SectionTitle
+        subHeading={"Don't miss"}
+        Heading={"Todays Offer"}
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+      {/* Dessert Menu */}
+      <MenuCategory items={dessert} title={'DESSERTS'} coverImg={DessertImg}></MenuCategory>
+      {/* Pizza Menu */}
+      <MenuCategory items={pizza} title={'PiZZA'} coverImg={PizzaImg}></MenuCategory>
+      {/* Salad Menu */}
+      <MenuCategory items={salad} title={'SALAD'} coverImg={SaladImg}></MenuCategory>
+      {/* Soup Menu */}
+      <MenuCategory items={soup} title={'SOUP'} coverImg={SoupImg}></MenuCategory>
+    </div>
+  );
 };
 
 export default Menu;
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -2,10 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const useMenu = () => {
-    const [menu,setMenu] = useState();
+    const [menu,setMenu] = useState([]);
     const [loading,setLoading] = useState(true)
     useEffect(()=>{
-        axios.get('./menu.json')
+        axios.get('menu.json')
         .then(data => {
             const loadedData = data.data
             setMenu(loadedData)
