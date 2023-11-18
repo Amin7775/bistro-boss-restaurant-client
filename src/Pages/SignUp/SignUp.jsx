615d40ac3738246528e01f408ae75155b06aa075
Amin7775 <amin.dpi24@gmail.com>
2023-11-18 22:34:49 +0600

    Using React Hook Form Validation, Error Handlingfor SignUp Page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,25 +1,34 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
 
-    const {createUser} = useContext(AuthContext)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-    
-        console.log(email, password);
-        createUser(email,password)
-        .then(result => {
-          const user = result.user;
-          console.log(user)
-        })
-      };
-    return (
-        <div className="hero min-h-screen bg-base-200">
+  const onSubmit = (data) => console.log(data);
+
+  // const handleSignUp = (e) => {
+  //     e.preventDefault();
+  //     const form = e.target;
+  //     const email = form.email.value;
+  //     const password = form.password.value;
+
+  //     console.log(email, password);
+  //     createUser(email,password)
+  //     .then(result => {
+  //       const user = result.user;
+  //       console.log(user)
+  //     })
+  //   };
+  return (
+    <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center md:w-1/2 lg:text-left">
           <h1 className="text-5xl font-bold">Sign Up</h1>
@@ -30,7 +39,20 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card  md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleSignUp} className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+                name="name"
+                {...register("name",{ required: true })}
+                />
+                {errors.name && <span>Name is required</span>}
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -40,8 +62,10 @@ const SignUp = () => {
                 placeholder="email"
                 className="input input-bordered"
                 name="email"
-                required
+                {...register("email",{ required: true })}
+                
               />
+              {errors.email && <span>Email is required</span>}
             </div>
             <div className="form-control">
               <label className="label">
@@ -52,25 +76,25 @@ const SignUp = () => {
                 placeholder="password"
                 className="input input-bordered"
                 name="password"
-                required
+                {...register("password",{ required: true })}
+                
               />
+              {errors.password && <span>Password is required</span>}
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
                 </a>
               </label>
             </div>
-            
+
             <div className="form-control mt-6">
-              <button className="btn btn-primary">
-                Login
-              </button>
+              <button className="btn btn-primary">Login</button>
             </div>
           </form>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default SignUp;
