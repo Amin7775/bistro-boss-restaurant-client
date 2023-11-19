a6b033f58c868d4e2bfdc006c1c15405350a2442
Amin7775 <amin.dpi24@gmail.com>
2023-11-20 01:25:57 +0600

    Cart data from custom hook and calculate total price and table

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,16 +1,26 @@
-import { FaCalendar, FaHome, FaList, FaShoppingCart, FaStar } from "react-icons/fa";
+import { FaCalendar, FaHome, FaHouseUser, FaList, FaMailBulk, FaShoppingBag, FaShoppingCart, FaStar } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div className="flex">
-            <div className="w-64 bg-orange-400 min-h-full">
-                <ul className="menu">
+        <div className="flex bg-[#F6F6F6]">
+            <div className="w-64 bg-orange-400 min-h-screen">
+                <div className="text-3xl ml-6 mt-10">
+                <h1 className="font-bold">Bistro Boss</h1>
+                <h1 className="font-semibold">Restaurant</h1>
+                </div>
+                <ul className="menu mt-10 text-lg">
                     <li><NavLink to={'/dashboard/userHome'}><FaHome></FaHome>User Home</NavLink></li>
                     <li><NavLink to={'/dashboard/reservation'}><FaCalendar></FaCalendar> Reservation</NavLink></li>
                     <li><NavLink to={'/dashboard/cart'}><FaShoppingCart></FaShoppingCart>My Cart</NavLink></li>
                     <li><NavLink to={'/dashboard/reviews'}><FaStar></FaStar>My Reviews</NavLink></li>
                     <li><NavLink to={'/dashboard/bookings'}><FaList></FaList>My Bookings</NavLink></li>
+
+                    <div className="divider"></div>
+                    <li><NavLink to={'/'}><FaHouseUser></FaHouseUser> Home</NavLink></li>
+                    <li><NavLink to={'/menu'}><FaList></FaList> Menu</NavLink></li>
+                    <li><NavLink to={'/order/salad'}><FaShoppingBag></FaShoppingBag> Shop</NavLink></li>
+                    <li><NavLink to={'/contact'}><FaMailBulk></FaMailBulk> Contact</NavLink></li>
                 </ul>
             </div>
             <div className="flex-1">
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,87 @@
+import useCart from "../../../hooks/useCart";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
 const Cart = () => {
-    return (
-        <div>
-            <h2 className="text-6xl"> My Cart</h2>
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((previous, current) => {
+    return (current = previous + current.price);
+  }, 0);
+  return (
+    <div>
+      <div className="my-8">
+        <SectionTitle
+          Heading={"manage all items"}
+          SubHeading={"Hurry Up"}
+        ></SectionTitle>
+      </div>
+      <div className="w-5/6 bg-white mx-auto min-h-[70vh]">
+        <div className="p-10 flex justify-between">
+          <h1 className="text-3xl font-medium">Total Orders : {cart.length}</h1>
+          <h1 className="text-3xl font-medium">Total Price : {totalPrice}</h1>
+          <button className="btn bg-orange-500 text-white text-xl px-8 ">
+            Pay
+          </button>
         </div>
-    );
+        <div className="w-full mx-auto px-5">
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <th>image</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src="/tailwind-css-component-profile-2@56w.png"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">Hart Hagerty</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    Zemlak, Daniel and Leannon
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      Desktop Support Technician
+                    </span>
+                  </td>
+                  <td>Purple</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+                </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
